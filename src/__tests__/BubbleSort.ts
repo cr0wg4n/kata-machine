d1254b69fd3eb568df0a11476e1a76baea561ebb
cr0wg4n <mcm.crw@gmail.com>
2023-03-23 16:01:34 -0400

    feat: two forms bubble short

--- src/__tests__/BubbleSort.ts
+++ src/__tests__/BubbleSort.ts
@@ -8,3 +8,10 @@ test("bubble-sort", function () {
     expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
 });
 
+test("bubble-sort-2", function () {
+    const arr = [1000, 23, 32, 9, 3, 7, 4, 69, 420, 42, 1];
+
+    debugger;
+    bubble_sort(arr);
+    expect(arr).toEqual([1, 3, 4, 7, 9, 23, 32, 42, 69, 420, 1000]);
+});
--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,28 @@
 export default function bubble_sort(arr: number[]): void {
+  let counter = 0
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      counter++
+      if(arr[i] > arr[j]) {
+        const tmp = arr[j]
+        arr[j] = arr[i]
+        arr[i] = tmp
+      }
+    }
+  }
 
+  console.log(counter)
+  counter=0
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length -1 -i; j++) {
+      counter++
+      if(arr[j] > arr[j+1]) {
+        const tmp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = tmp
+      }
+    }
+  }
+  console.log(counter)
 }
