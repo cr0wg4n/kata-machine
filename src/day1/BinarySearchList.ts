adf822171ecc13c9768b8730f9a128e58ea8b477
cr0wg4n <mcm.crw@gmail.com>
2023-03-23 15:11:30 -0400

    feat: generated and 3 solved

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,16 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+  let low = 0;
+  let high = haystack.length;
+  do {
+    const middle = Math.floor(low + (high-low)/2)
+    const value = haystack[middle]
+    if(needle === value) {
+      return true
+    } else if(value > needle) {
+      high = middle 
+    } else {
+      low = middle + 1
+    }
+  } while (low < high)
+  return false
+}
