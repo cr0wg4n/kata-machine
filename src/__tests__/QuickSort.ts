821b533e08499398b0c271d65db6023803074bef
cr0wg4n <mcm.crw@gmail.com>
2023-05-02 18:22:19 -0400

    feature: quicksort implementation

--- src/__tests__/QuickSort.ts
+++ src/__tests__/QuickSort.ts
@@ -4,6 +4,5 @@ test("quick-sort", function () {
     const arr = [9, 3, 7, 4, 69, 420, 42];
 
     debugger;
-    quick_sort(arr);
-    expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+    expect(quick_sort(arr)).toEqual([3, 4, 7, 9, 42, 69, 420]);
 });
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,14 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[]): number[] {
+  if(arr.length < 2) {
+    return arr
+  } else {
+    const pivot = arr[0]
+    const less = arr.slice(1, arr.length).filter(i => i <= pivot)
+    const greater = arr.slice(1, arr.length).filter(i => i > pivot)
+    return [...qs(less), pivot, ...qs(greater)]
+  }
+}
 
+export default function quick_sort(arr: number[]): number[] {
+  return qs(arr)
 }
